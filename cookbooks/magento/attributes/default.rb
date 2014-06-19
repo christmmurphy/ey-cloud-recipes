918bb519d56c94b6f55aeea0fea0005c2adea40e
Chris Murphy <cmurphy@engineyard.com>
2014-06-19 16:21:32 +0100

    pushing up to date repo

---
+++ cookbooks/magento/attributes/default.rb
@@ -0,0 +1,5 @@
+default[:magento] = {
+  :encryption_key => "d4e5ae7cf987c4f860188b6518496e4f",   # Add your encryption key here
+  :redis_session_store => true,          # Set to true to enable Redis session storage
+  :redis_page_caching => true            # Set to true to enable Redis page caching
+}
